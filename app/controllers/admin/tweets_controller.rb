ea88ca2c1b22aae4170360ab5bf8cb85469ade3c
josher <josher01.lin@gmail.com>
2018-03-05 17:12:49 +0800

    add navbar and set up log out function

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -1,4 +1,17 @@
 class Admin::TweetsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :authenticate_admin
   def index
+  end
+
+
+
+  private
+  def authenticate_admin
+    if current_user.role == 'admin'
+      redirect_to admin_tweets_path
+    else
+      redirect_back(fallback_location: root_path, alert: "You are Not admin !")
+    end
   end
 end
