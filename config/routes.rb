4d0ca7ef533e3edb452c08aa1ef7fc6b46c1b6b5
josher <josher01.lin@gmail.com>
2018-03-05 15:31:59 +0800

    set up user model with devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root 'tweets#index'
   resources :tweets, only: [:index, :create] do
     resources :replies, only: [:index,:create]
   end
 
   namespace :admin do
     resources :tweets
+    root 'tweets#index'
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
