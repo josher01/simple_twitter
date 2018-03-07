b61268115c4dd64b0a7beaaca8e245582f7fee8b
josher <josher01.lin@gmail.com>
2018-03-07 14:26:35 +0800

    add username column for sign up view and user can tweet in index page

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,8 +1,29 @@
 class TweetsController < ApplicationController
-  before_action :authenticate_user!
+  
   def index
+    @tweets = Tweet.order(created_at: :desc).first(5)
+    @tweet = Tweet.new
   end
 
+
   def create
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      flash[:notice] = 'Tweet Created Successfully !'
+      redirect_to tweets_path
+    else
+      flash[:alert] = @tweet.error.messages
+      redirect_to tweets_path
+    end
+  end
+
+  def show
+
+  end
+
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:description)
   end
 end
